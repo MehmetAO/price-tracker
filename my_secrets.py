d333199cf0c54091816ebfd1ac0329812c272d02
Mehmet Ali <109021192+MehmetAO@users.noreply.github.com>
2022-07-24 17:17:15 +0300

    Update my_secrets.py

--- my_secrets.py
+++ my_secrets.py
@@ -1,4 +1,4 @@
-TWILIO_ACCOUNT_SID = "***"
-TWILIO_AUTH_TOKEN = "***"
-TWILIO_FROM_NUMBER = "+***"
-MY_PHONE_NUMBER = "+905***"
+TWILIO_ACCOUNT_SID = "***"  # Change before commit
+TWILIO_AUTH_TOKEN = "***"  # Change before commit
+TWILIO_FROM_NUMBER = "+***"  # Change before commit
+MY_PHONE_NUMBER = "+905***"  # Change before commit
